0419163a3a87c5b8723dd4937ba4bc7f8d04d512
Shivam <shivamtech431@gmail.com>
2025-01-07 18:47:26 +0530

    factorial USing Recursion

--- Recursion/Factorial.java
+++ Recursion/Factorial.java
@@ -11,7 +11,7 @@ public class Factorial {
     }
 
     public static int fact(int n) {
-        if(n==1) return 1;
+        if(n==1 || n==0) return 1;
         int ans = n*fact(n-1);
         return ans;
     }
