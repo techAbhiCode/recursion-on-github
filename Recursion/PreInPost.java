26751af2751e692febe690087370bb2a8acc0caa
Shivam <shivamtech431@gmail.com>
2025-01-09 01:14:14 +0530

    Pre in Post is type of program that we can use to understand the call Stack for binary trees

---
+++ Recursion/PreInPost.java
@@ -0,0 +1,21 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class PreInPost {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int n = in.nextInt();
+        pip(n);
+    }
+
+    public static void pip(int n) {
+        if (n == 0) return;
+        System.out.println(n);  //Pre
+        pip(n - 1);
+        System.out.println(n); // In
+        pip(n - 1);
+        System.out.println(n);  //post
+    }
+}
