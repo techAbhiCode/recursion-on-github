5ecbaf2284c14583bf45933cb0f61a494ca5835b
Shivam <shivamtech431@gmail.com>
2025-01-09 00:07:55 +0530

    total number of ways in a maze type path like matrix mxn from 1,1 to m,n

---
+++ Recursion/mazePath.java
@@ -0,0 +1,21 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class mazePath {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter m: ");
+        int m = in.nextInt();
+        System.out.println("Enter n: ");
+        int n = in.nextInt();
+        System.out.println(maze(1,1,m,n));
+    }
+
+    public static int maze(int row, int col, int m, int n) {
+        if (row == m || col == n) return 1;
+        int rightWays = maze(row, col+1, m, n);
+        int downWays = maze(row+1, col, m, n);
+        return rightWays + downWays;
+    }
+}
