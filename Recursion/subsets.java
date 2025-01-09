11c1da517ec64a257a856fbb09f2a56572801235
Shivam <shivamtech431@gmail.com>
2025-01-09 16:13:48 +0530

    getting subsets of a string using recursion and Arraylist

--- Recursion/subsets.java
+++ Recursion/subsets.java
@@ -1,18 +1,25 @@
 package Recursion;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class subsets {
+    static ArrayList<String> arr = new ArrayList<>();
     public static void main(String[] args) {
         String s = "abcd";
         printSubsets(0,s,"");
+        System.out.println(arr);
     }
 
     public static void printSubsets(int i, String s, String ans) {
         if (i == s.length()) {
-            System.out.println(ans);
+            arr.add(ans);
             return;
         }
         char ch = s.charAt(i);
-        printSubsets(i+1, s, ans+ch);  //take
         printSubsets(i+1, s, ans);                 //not take
+        printSubsets(i+1, s, ans+ch);  //take
+
+
     }
 }
